74ac82922ef170bf8eda752e64bc0bda07bf5871
swa07016 <swa07016@naver.com>
2020-02-04 16:17:00 +0900

    Creating Queries with Arguments

--- graphql/db.js
+++ graphql/db.js
@@ -13,6 +13,6 @@ export const people = [
 ];
 
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === people.id);
+    const filteredPeople = people.filter(person => id === person.id);
     return filteredPeople[0];
 }
