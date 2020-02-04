84529b85760ec6587d93759bf035c845d2e5727f
swa07016 <swa07016@naver.com>
2020-02-04 17:02:37 +0900

    Creating first Mutation

--- graphql/db.js
+++ graphql/db.js
@@ -1,18 +1,44 @@
-export const people = [
+let movies = [
+    {
+        id: 0,
+        name: "Star Wars - The new one",
+        score: 1
+    },
     {
         id: 1,
-        name: "nicolas",
-        age: 20,
-        gender: "female"
-    }, 
-    {   id: 2,
-        name: "barnacle_lou", 
-        age: 5,
-        gender: "female"
+        name: "Avengers - The new one",
+        score: 8
+    },
+    {
+        id: 2,
+        name: "The Godfather I",
+        score: 99
     }
 ];
 
+export const getMovies = () => movies;
+
 export const getById = id => {
-    const filteredPeople = people.filter(person => id === person.id);
-    return filteredPeople[0];
+    const filteredMovies = movies.filter(movie => id === movie.id);
+    return filteredMovies[0];
 }
+
+export const deleteMovie = (id) => {
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
+    if (movies.length > cleanedMovies.length) {
+        movies = cleanedMovies;
+        return true;
+    } else {
+        return false;
+    }
+}
+
+export const addMovie = (name, score) => {
+    const newMovie = {
+        id: movies.length + 1,
+        name,
+        score
+    };
+    movies.push(newMovie);
+    return newMovie;
+};	
